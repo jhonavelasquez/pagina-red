21cf61dfc1921cf493aec11551811c6f5c08214d
jona <jon.velasquez@duocuc.cl>
2024-07-09 02:52:22 -0400

    paradas en mapa

--- core/models.py
+++ core/models.py
@@ -1,22 +1,12 @@
 from django.db import models
 
-# Create your models here.
-from django.conf import settings
-from django.utils import timezone
+class Route(models.Model):
+    name = models.CharField(max_length=100)
+    # Agrega otros campos según sea necesario
 
-
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
+class Stop(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # Agrega otros campos según sea necesario
--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from google.cloud import bigquery
+import re
+import json  # Asegúrate de importar el módulo json
 
-# Create your views here.
+def map_view(request):
+    # Configura el cliente de BigQuery
+    client = bigquery.Client()
+
+    # Escribe tu consulta de BigQuery para obtener las rutas y paradas
+    query = """
+    SELECT codsint, pos, name, cod, tipo_recorrido, comuna, color, type
+    FROM `project-red-423402.dataset_realtime.paraderos`
+    """
+    query_job = client.query(query)
+    rows = query_job.result()
+
+    # Convierte los resultados en una lista de diccionarios
+    paraderos = []
+    for row in rows:
+        # Parsear las coordenadas si vienen como un string
+        if isinstance(row.pos, str):
+            coords_match = re.match(r'\[(-?\d+\.\d+), (-?\d+\.\d+)\]', row.pos)
+            if coords_match:
+                coords = [float(coords_match.group(1)), float(coords_match.group(2))]
+            else:
+                coords = [None, None]
+        else:
+            coords = [None, None]
+
+        paraderos.append({
+            'codsint': row.codsint,
+            'coords': coords,
+            'name': row.name,
+            'code': row.cod,
+            'description': f"{row.tipo_recorrido}, {row.comuna}, {row.color}, {row.type}"
+        })
+
+    context = {
+        'paraderos_json': json.dumps(paraderos),
+    }
+
+    return render(request, 'core/map.html', context)
